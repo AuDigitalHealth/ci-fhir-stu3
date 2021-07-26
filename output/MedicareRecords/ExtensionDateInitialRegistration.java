4de5b96347c97e4111de6ecda865ec0323747a16
Rob Eastwood <rob.eastwood@digitalhealth.gov.au>
2021-07-26 10:11:08 +1000

    Remove all generated outputs for Medicare Records IG

--- output/MedicareRecords/ExtensionDateInitialRegistration.java
+++
@@ -1,7 +0,0 @@
-package http://ns.electronichealth.net.au/ci/fhir/ImplementationGuide/implementationguide-medicare-records;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class ExtensionDateInitialRegistration {
-
-}
